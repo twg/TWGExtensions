f2f52f466abc5ef4cbebe6d6f325e2fc3e69d1e0
John Grant <johnkgrant@gmail.com>
2014-08-21 14:40:37 -0400

    Add nib-in-nib loading helper

--- Pod/Classes/UIView/UIView+TWGNib.h
+++ Pod/Classes/UIView/UIView+TWGNib.h
@@ -6,4 +6,15 @@
 + (id)twg_loadNibViewFromClass:(Class)class;
 + (id)twg_loadNibViewWithName:(NSString *)name;
 
+/**
+ * Use when loading views from a nib inside another nib
+ * Make sure you set the tag to '999' of the view in the nib
+ * Put the following in the subviews class:
+ - (id)awakeAfterUsingCoder:(NSCoder *)aDecoder
+ {
+ return [self replacePlaceholderWithAutolayout:YES coder:aDecoder];
+ }
+ */
+- (UIView *)twg_replacePlaceholderWithAutolayout:(BOOL)autolayout coder:(NSCoder *)aDecoder;
+
 @end
--- Pod/Classes/UIView/UIView+TWGNib.m
+++ Pod/Classes/UIView/UIView+TWGNib.m
@@ -1,6 +1,8 @@
 
 #import "UIView+TWGNib.h"
 
+const NSInteger TWGNibLoadingReferenceTag = 999;
+
 @implementation UIView (TWGNib)
 
 + (instancetype)twg_loadNib
@@ -16,6 +18,28 @@
 + (id)twg_loadNibViewWithName:(NSString *)name
 {
     return [[[NSBundle mainBundle] loadNibNamed:name owner:nil options:nil] firstObject];
+}
+
+- (UIView *)twg_replacePlaceholderWithAutolayout:(BOOL)autolayout coder:(NSCoder *)aDecoder
+{
+    if (self.tag == TWGNibLoadingReferenceTag) {
+        UIView *realView = [[self class] twg_loadNib];
+        realView.frame = self.frame;
+        realView.alpha = self.alpha;
+        realView.autoresizingMask = self.autoresizingMask;
+        realView.autoresizesSubviews = self.autoresizesSubviews;
+        
+        if (autolayout) {
+            realView.translatesAutoresizingMaskIntoConstraints = NO;
+            [realView addConstraints:self.constraints];
+        }
+
+        for (UIView *view in self.subviews) {
+            [realView addSubview:view];
+        }
+        return realView;
+    }
+    return [super awakeAfterUsingCoder:aDecoder];
 }
 
 @end

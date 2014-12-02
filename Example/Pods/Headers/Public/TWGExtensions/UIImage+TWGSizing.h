e14c1884daa9194c6a124110b988dc0992c2233f
John Grant <johnkgrant@gmail.com>
2014-12-02 09:22:36 -0500

    Update pods

---
+++ Example/Pods/Headers/Public/TWGExtensions/UIImage+TWGSizing.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/UIImage/UIImage+TWGSizing.h
---
+++ Example/Pods/Target Support Files/Pods-Tests/Pods-Tests-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Expecta
+#define COCOAPODS_POD_AVAILABLE_Expecta
+#define COCOAPODS_VERSION_MAJOR_Expecta 0
+#define COCOAPODS_VERSION_MINOR_Expecta 3
+#define COCOAPODS_VERSION_PATCH_Expecta 1
+
+// FBSnapshotTestCase
+#define COCOAPODS_POD_AVAILABLE_FBSnapshotTestCase
+#define COCOAPODS_VERSION_MAJOR_FBSnapshotTestCase 1
+#define COCOAPODS_VERSION_MINOR_FBSnapshotTestCase 4
+#define COCOAPODS_VERSION_PATCH_FBSnapshotTestCase 0
+
+// TWGExtensions
+#define COCOAPODS_POD_AVAILABLE_TWGExtensions
+#define COCOAPODS_VERSION_MAJOR_TWGExtensions 0
+#define COCOAPODS_VERSION_MINOR_TWGExtensions 1
+#define COCOAPODS_VERSION_PATCH_TWGExtensions 3
+
